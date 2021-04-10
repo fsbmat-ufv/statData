1d40da0bd7fce26fea8122b90acfe957ac954393
fsbmat <fsbmat@gmail.com>
2021-04-09 21:27:22 -0300

    Update1

---
+++ R/df12.R
@@ -0,0 +1,31 @@
+#' @name df12
+#'
+#' @title Resumo de Dados
+#'
+#' @description Descrever a atividade.
+#'
+#' @format Um \code{data.frame} com xx observacoes e 2 variaveis, em que
+#'
+#' \describe{
+#'
+#' \item{\code{Nome da Variavel 1}}{Descricao da variavel}
+#'
+#' \item{\code{Nome da Variavel 1}}{Descricao da variavel}
+#'
+#' }
+#' @keywords Digite tres palavras chave
+#'
+#'
+#' @source Referencia Bibliografica
+#'
+#' @examples
+#'#Fazer a leitura do Banco de Dados
+#'data(df12)
+#'#Para visualizar a Estrutura e os tipos de variaveis do Banco de dados faca:
+#'str(df12)
+#'#Para visualizar os nomes das colunas do banco de dados faca:
+#'names(df12)
+#'#'#Caso queira ver um resumo do conjunto de dados:
+#'summary(df12)
+#'
+NULL
---
+++ R/df3.R
@@ -0,0 +1,31 @@
+#' @name df3
+#'
+#' @title Resumo de Dados
+#'
+#' @description Descrever a atividade.
+#'
+#' @format Um \code{data.frame} com xx observacoes e 2 variaveis, em que
+#'
+#' \describe{
+#'
+#' \item{\code{Nome da Variavel 1}}{Descricao da variavel}
+#'
+#' \item{\code{Nome da Variavel 1}}{Descricao da variavel}
+#'
+#' }
+#' @keywords Digite tres palavras chave
+#'
+#'
+#' @source Referencia Bibliografica
+#'
+#' @examples
+#'#Fazer a leitura do Banco de Dados
+#'data(df3)
+#'#Para visualizar a Estrutura e os tipos de variaveis do Banco de dados faca:
+#'str(df3)
+#'#Para visualizar os nomes das colunas do banco de dados faca:
+#'names(df3)
+#'#'#Caso queira ver um resumo do conjunto de dados:
+#'summary(df3)
+#'
+NULL
